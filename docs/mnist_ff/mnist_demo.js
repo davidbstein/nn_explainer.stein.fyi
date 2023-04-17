3401caa7037ce782bfb17f1f6acb1871effcacc5
David Stein <davidbstein@gmail.com>
2023-04-16 21:06:18 -0400

    Preparing to add batching...

--- docs/mnist_ff/mnist_demo.js
+++ docs/mnist_ff/mnist_demo.js
@@ -1,173 +1,9 @@
-class Neuron {
-  constructor(inputCount) {
-    this.weights = [];
-    for (let i = 0; i < inputCount; i++) {
-      this.weights.push((Math.random() * 2 - 1)/2);
-    }
-    this.bias = 0;
-    this.forward= this.forwardSigmoid;
-    this.backward= this.backwardSigmoid;
-    // this.forward = this.forwardReLU;
-    // this.backward = this.backwardReLU;
-  }
-
-  forwardSigmoid(inputs) {
-    // sigmoid activation function
-    let sum = 0;
-    for (let i = 0; i < inputs.length; i++) {
-      sum += inputs[i] * this.weights[i];
-    }
-    sum += this.bias;
-    return 1 / (1 + Math.exp(-sum));
-  }
-
-  /**
-   * @param {*} inputs - the values output by the neurons in the previous layer
-   * @param {*} outputGradient - the gradient of the loss function with respect to the output of this neuron
-   * @param {*} learningRate - the learning rate
-   * @returns the gradient of the loss function with respect to the inputs of this neuron
-   */
-  backwardSigmoid(inputs, outputGradient, learningRate) {
-    let weightedSum = inputs.reduce((sum, input, i) => sum + input * this.weights[i], this.bias);
-    let sigmoidDerivative = (1 / (1 + Math.exp(-weightedSum))) * (1 - 1 / (1 + Math.exp(-weightedSum)));
-    let weightedSumGradient = outputGradient * sigmoidDerivative;
-    let biasGradient = weightedSumGradient;
-    this.bias -= learningRate * biasGradient;
-    let weightGradients = this.weights.map((_, i) => weightedSumGradient * inputs[i]);
-    for (let i = 0; i < this.weights.length; i++) {
-      this.weights[i] -= learningRate * weightGradients[i];
-    }
-    let inputGradients = this.weights.map(weight => weightedSumGradient * weight);  
-    return inputGradients;
-  }
-
-  forwardReLU(inputs) {
-    let sum = 0;
-    for (let i = 0; i < inputs.length; i++) {
-      sum += inputs[i] * this.weights[i];
-    }
-    sum += this.bias;
-    return Math.max(0, sum);
-  }
-
-  backwardReLU(inputs, outputGradient, learningRate) {
-    let weightedSum = inputs.reduce((sum, input, i) => sum + input * this.weights[i], this.bias);
-    let reluDerivative = weightedSum > 0 ? 1 : 0;
-    let weightedSumGradient = outputGradient * reluDerivative;
-    let biasGradient = weightedSumGradient;
-    this.bias -= learningRate * biasGradient;
-    let weightGradients = this.weights.map((_, i) => weightedSumGradient * inputs[i]);
-    for (let i = 0; i < this.weights.length; i++) {
-      this.weights[i] -= learningRate * weightGradients[i];
-    }
-    let inputGradients = this.weights.map(weight => weightedSumGradient * weight);
-    return inputGradients;
-  }
-
-  toString() {
-    return JSON.stringify({
-      weights: this.weights,
-      bias: this.bias,
-    });
-  }
-}
-
-class Layer {
-  constructor(neuronCount, inputCount) {
-    this.neurons = [];
-    for (let i = 0; i < neuronCount; i++) {
-      let neuron = new Neuron(inputCount);
-      this.neurons.push(neuron);
-    }
-  }
-
-  forward(inputs) {
-    let outputs = [];
-    for (let i = 0; i < this.neurons.length; i++) {
-      outputs.push(this.neurons[i].forward(inputs));
-    }
-    return outputs;
-  }
-
-  backward(inputs, outputGradients, learningRate) {
-    let inputGradients = [];
-    for (let i = 0; i < inputs.length; i++) {
-      inputGradients.push(0);
-    }
-    for (let i = 0; i < this.neurons.length; i++) {
-      let neuronInputGradients = this.neurons[i].backward(
-        inputs,
-        outputGradients[i],
-        learningRate
-      );
-      for (let j = 0; j < inputs.length; j++) {
-        inputGradients[j] += neuronInputGradients[j];
-      }
-    }
-    return inputGradients;
-  }
-
-  toString() {
-    let to_ret = `Layer <${this.neurons.length} neurons>`
-    for (let i = 0; i < this.neurons.length; i++) {
-      to_ret += this.neurons[i].toString()
-    }
-    return to_ret;
-  }
-}
-
-class Network {
-  constructor(inputCount, hiddenLayerSizes, outputCount) {
-    this.layers = [];
-    let prevLayerSize = inputCount;
-    for (let i = 0; i < hiddenLayerSizes.length; i++) {
-      let layer = new Layer(hiddenLayerSizes[i], prevLayerSize);
-      this.layers.push(layer);
-      prevLayerSize = hiddenLayerSizes[i];
-    }
-    this.layers.push(new Layer(outputCount, prevLayerSize));
-  }
-
-  computeInternals(inputs) {
-    let to_ret = [inputs];
-    let outputs = inputs;
-    for (let i = 0; i < this.layers.length; i++) {
-      outputs = this.layers[i].forward(outputs);
-      to_ret.push(outputs);
-    }
-    return to_ret;
-  }
-
-  forward(inputs) {
-    return this.computeInternals(inputs)[this.layers.length - 1];
-  }
-
-  backward(inputs, label, learningRate) {
-    let targetOutput = []
-    for (let i = 0; i < 10; i++) {
-      targetOutput.push(i===label ? 1 : 0);
-    }
-    let outputs = this.computeInternals(inputs);
-    let outputGradients = [];
-    for (let i = 0; i < outputs[outputs.length - 1].length; i++) {
-      const output = outputs[outputs.length - 1][i];
-      const target = targetOutput[i];
-      outputGradients.push((output - target) * (output > 0 ? 1 : 0));
-    }
-    for (let i = this.layers.length - 1; i >= 0; i--) {
-      outputGradients = this.layers[i].backward(
-        outputs[i],
-        outputGradients,
-        learningRate
-      );
-    }
-  }
-}
-
 function drawNeuronWeights(target, weights, width, height) {
   let canvas = document.createElement("canvas");
   canvas.width = width;
   canvas.height = height;
+  const ratio = height/width;
+  canvas.style = `width: ${width}em; height: ${height}em;`
   let ctx = canvas.getContext("2d");
   let imageData = ctx.getImageData(0, 0, width, height);
   let data = imageData.data;
@@ -237,6 +73,11 @@ function drawHiddenLayer(target, layer, outputs) {
   target.appendChild(div);
 }
 
+function normalizeOutputVal(val){
+  const [minVal, maxVal] = window.RANGE;
+  return (val - minVal) / (maxVal - minVal);
+}
+
 /**
  * draws the output of the network into the "output" div, represented as a column of 10 circles, colored black if the probability is 0, and white if the probability is 1.
  * each circle is labeled with the probability of that digit, formatted to 2 decimal places.
@@ -246,7 +87,8 @@ function drawOutput(outputs) {
   target = document.getElementById("output");
   for (let i = 0; i < 10; i++) {
     let div = document.createElement("div");
-    let outputColor = Math.min(outputs[i], 1);
+    let val = normalizeOutputVal(outputs[i]);
+    let outputColor = Math.min(val, 1);
     outputColor = 1 - (1-outputColor) * (1-outputColor);
     outputColor *= 255;
     div.innerHTML = (`
@@ -295,10 +137,9 @@ function instrumentInputImage(redraw){
     elem.addEventListener("mouseover", (e) => {
       const mouseDown = window.mouseDown;
       const pixelData = window.currentInput;    
-      let delta = -.15;
-      if (mouseDown) {
-        delta = 0.05;
-      }
+      const drawMode = window.drawMode;
+      if (!mouseDown) return;
+      let delta = drawMode ? 0.05 : -0.15;
       let rect = e.target.getBoundingClientRect();
       let pixelIdx = parseInt(e.target.dataset.pixelidx);
       pixelData[pixelIdx] = Math.max(0, Math.min(pixelData[pixelIdx]+delta, 1));
@@ -338,9 +179,11 @@ function drawInputImage(target, pixelData) {
  */
 function drawNetwork(network, inputs) { 
   const inputTarget = document.getElementById("input-image");
+  const inputLabelTarget = document.getElementById("input-label");
   const hiddenTarget = document.getElementById("hidden-layers");
   const outputTarget = document.getElementById("output");
   inputTarget.innerHTML = "";
+  inputLabelTarget.innerHTML = window.currentLabel;
   hiddenTarget.innerHTML = "";
   outputTarget.innerHTML = "";
   drawInputImage(inputTarget, inputs);
@@ -352,33 +195,88 @@ function drawNetwork(network, inputs) {
 }
 
 
+function storeNetworkInLocalStorage(network) {
+  let layers = [];
+  for (let i = 0; i < network.layers.length; i++) {
+    let layer = [];
+    for (let j = 0; j < network.layers[i].neurons.length; j++) {
+      layer.push({
+        weights: network.layers[i].neurons[j].weights,
+        bias: network.layers[i].neurons[j].bias
+      });
+    }
+    layers.push(layer);
+  }
+  localStorage.setItem("network", JSON.stringify(layers));
+  console.log("STORED");
+}
+
+function restoreNetworkWeightsFromLocalStorage(network) {
+  let layers = JSON.parse(localStorage.getItem("network"));
+  let count = 0;
+  for (let i = 0; i < network.layers.length; i++) {
+    if (layers.length <= i) continue;
+    for (let j = 0; j < network.layers[i].neurons.length; j++) {
+      if (layers[i].length <= j) continue;
+      count++
+      network.layers[i].neurons[j].weights = layers[i][j].weights;
+      network.layers[i].neurons[j].bias = layers[i][j].bias;
+    }
+  }
+  console.log(`RESTORED ${count} neurons`);
+}
+
+function linearProjection(vector, projectionMatrix) {
+  if (vector.length === 2) 
+    return vector.map(normalizeOutputVal);
+  let result = [0, 0];
+  // linear projection
+  for (let i = 0; i < vector.length; i++) {
+    result[0] += vector[i] * projectionMatrix[i][0];
+    result[1] += vector[i] * projectionMatrix[i][1];
+  }
+  // Normalize
+  result[0] = Math.abs(result[0]) % 1;
+  result[1] = Math.abs(result[1]) % 1;
+  return result.map(normalizeOutputVal);
+}
 
 
 window.onload = async function () {
-
-  // setup a network that takes 784 inputs and outputs 10 values. 
-  // By default has 2 hidden layers with 16 neurons each, but this can be overridden by including a comma separated list of numbers in the url (e.g. ?layers=8,8,8)
-  let layers = [8, 8];
-  if (window.location.search) {
-    layers = window.location.search.substring(1).split(",").map(x => parseInt(x));
+  /**
+   * output: {
+   *   image: a 784 number array between 0 and 1, representing an 28x28 image where 0 is black and 1 is white
+   *   label: the digit represented by the image
+   * }
+   * returns a black image if there are no valid digits, with the label -1
+   */
+  function getRandomImage() {
+    const validDigits = window.currentDigits;
+    if (validDigits.length === 0) {
+      return {
+        image: Array(784).fill(0),
+        label: -1
+      }
+    }
+    let index = Math.floor(Math.random() * data.length);
+    while (!validDigits.includes(data[index].label)) {
+      index = Math.floor(Math.random() * data.length);
+    }
+    return data[index];
   }
-  let network = new Network(784, layers, 10);
 
-  // data is in a json file at /mnist_handwritten_train.json, which is a list of objects of the form {image: [784 ints, between 0 and 255], label: [int, between 0 and 9]}
-  // let train_data = MNIST_TRAIN;
-  let data = MNIST_TEST;
+
 
   function loadRandomImage() {
-    let index = Math.floor(Math.random() * data.length);
-    window.currentInput = data[index].image.map(x => x / 255);
-    window.currentLabel = data[index].label;
+    const imageData = getRandomImage();
+    window.currentInput = imageData.image.map(x => x / 255);
+    window.currentLabel = imageData.label;
     redraw();
   }
 
   function clearImage() {
-    let index = Math.floor(Math.random() * data.length);
-    window.currentInput = data[index].image.map(x => 0);
-    window.currentLabel = data[index].label;
+    window.currentInput = Array(784).fill(0);
+    window.currentLabel = -1;
     redraw();
   }
 
@@ -395,16 +293,29 @@ window.onload = async function () {
     redraw();
   }
 
+  function resetWeights() {
+    network.resetWeights();
+    redraw();
+  }
+
+  function loadNewLayers() {
+    const layers = document.getElementById("layers-input").value.split(",").map(x => parseInt(x));
+    window.history.pushState({}, "", `?${layers.join(",")}`);
+    network.changeLayersButRetainWeights(layers);
+    document.getElementById("layers-input").classList.remove("changed");
+    document.getElementById("set-layers").disabled = true;
+    redraw();
+  }
+
   function train(n) {
     console.log(`training for ${n} iterations...`)
     for (q of document.querySelectorAll("button")) {
       q.disabled = true;
     }
-    const progressbar = document.getElementById("progress-bar-filler");
     for (let i = 0; i < n; i++) {
-      let index = Math.floor(Math.random() * data.length);
-      let input = data[index].image.map(x => x / 255);
-      let label = data[index].label;
+      const imageData = getRandomImage();
+      let input = imageData.image.map(x => x / 255);
+      let label = imageData.label;
       let learningRate = window.LEARNING_RATE;
       network.backward(input, label, learningRate);
     }
@@ -414,20 +325,116 @@ window.onload = async function () {
     for (q of document.querySelectorAll("button")) {
       q.disabled = false;
     }
-    console.log("done training!")
+    console.log("done training!");
+    storeNetworkInLocalStorage(network);
   }
 
+
+  function updateCurrentDigits() {
+    window.currentDigits = [];
+    for (let elem of document.querySelectorAll("input[name=digit]")) {
+      if (elem.checked) {
+        window.currentDigits.push(parseInt(elem.value));
+      }
+    }
+  }
+
+  function toggleDrawErase() {
+    window.drawMode = !window.drawMode;
+    const button = document.getElementById("draw-erase-toggle");
+    button.innerHTML = window.drawMode ? "draw mode" : "erase mode";
+  }
+
+  /**
+   * START SIMULATOR
+   */
+  window.currentDigits = [0,1,2,3,4,5,6,7,8,9];
+  const _method_id = 2;
+  window.METHOD = ["sigmoid", "relu", "tanh"][_method_id];
+  window.RANGE = [[0, 1], [0,1000], [-1,1]][_method_id];
+  updateCurrentDigits();
   window.mouseDown = false;
+  window.drawMode = true;
   document.body.addEventListener("mousedown", () => window.mouseDown = true);
   document.body.addEventListener("mouseup", () => window.mouseDown = false);
+  window.LEARNING_RATE = .1;
+
+  let layers = [8, 8];
+  if (window.location.search) {
+    layers = window.location.search.substring(1).split(",").map(x => parseInt(x));
+  }
+  let network = new Network(784, layers, 10);
+  window.network = network;
+  let data = MNIST_TEST;
+  restoreNetworkWeightsFromLocalStorage(network);
+  loadRandomImage();
+
+
+  /**
+   * SETUP BUTTONS
+   */
 
   document.getElementById("load-random-image").addEventListener("click", loadRandomImage);
   document.getElementById("clear-image").addEventListener("click", clearImage);
+  document.getElementById("reset-weights").addEventListener("click", resetWeights);
   document.getElementById("update-weights").addEventListener("click", () => updateWeights(1));
   document.getElementById("update-weights-1000").addEventListener("click", () => updateWeights(10000));
   document.getElementById("train-1").addEventListener("click", () => train(1));
   document.getElementById("train-1000").addEventListener("click", () => train(1000));
   document.getElementById("train-100000").addEventListener("click", () => train(10000));
-  window.LEARNING_RATE = .1;
-  loadRandomImage();
+  document.getElementById("draw-erase-toggle").addEventListener("click", toggleDrawErase);
+  document.querySelectorAll("input[name=digit]").forEach((elem) => {
+    elem.addEventListener("change", updateCurrentDigits);
+  });
+
+  document.getElementById("layers-input").placeholder = `comma-seporated layers (e.g., ${layers.join(",")})`;
+  document.getElementById("layers-input").value = `${layers.join(",")}`;
+  document.getElementById("layers-input").onkeydown = (e) => {
+    document.getElementById("set-layers").disabled = false;
+    if (e.keyCode === 13) {
+      loadNewLayers();
+    }
+  }
+  document.getElementById("set-layers").addEventListener("click", loadNewLayers);
+
+  function imageToVec(input, label){
+    const outputs = network.computeInternals(input);
+    let layer = null;
+    for (let i = 0; i < outputs.length; i++) {
+      let currentLayer = outputs[i];
+      if (layer === null || currentLayer.length < layer.length) {
+        layer = currentLayer;
+      }
+    }
+    return layer;
+  }
+
+  /**
+   * SPACE VIZ
+   */
+  const space_viz = new SpaceViz(document.getElementById("space-viz"));
+  window.space_viz = space_viz;
+  function addSVDigit(redraw, image, label){
+    if (image === undefined || label === undefined) {
+      const imageData = getRandomImage();
+      image = imageData.image;
+      label = imageData.label;
+    }
+    const vec = imageToVec(image, label);
+    window.space_viz.addDigit(image, label, vec);
+    if (redraw != false) window.space_viz.draw();
+  }
+  function rotateSVProjections(){
+    window.space_viz.resetProjectionMatrix();
+    window.space_viz.draw();
+  }
+  document.getElementById("SV-add-current").addEventListener("click", () => {
+    addSVDigit(false, window.currentInput, window.currentLabel);
+  });
+  document.getElementById("SV-add-random").addEventListener("click", addSVDigit);
+  document.getElementById("SV-add-100").addEventListener("click", 
+    () => {for (let i = 0; i < 100; i++) addSVDigit(false);window.space_viz.draw();}
+  );
+  document.getElementById("SV-reset").addEventListener("click", () => {window.space_viz.reset();});
+  document.getElementById("SV-rotate").addEventListener("click", rotateSVProjections);
 }
